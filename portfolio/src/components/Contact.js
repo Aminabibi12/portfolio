7821145b5978ef451504cd12293453b7a3df12dc
occ-113 <occ-113@occ-113s-MacBook-Pro.local>
2024-02-28 00:42:40 +0100

    form designed

---
+++ portfolio/src/components/Contact.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import './Contact.css'; 
+
+const Contact = () => {
+  const [formData, setFormData] = useState({
+    fullName: '',
+    email: '',
+    message: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(formData);
+  
+    setFormData({ fullName: '', email: '', message: '' });
+  };
+
+  return (
+
+    <div className='contact-box' id="contact">
+        <p><b>If you want to work with me!</b></p>
+     <h2>Contact Me</h2>
+    <div className="contact-form-container">
+      <form onSubmit={handleSubmit} className="contact-form">
+        <div className="form-group">
+          <label htmlFor="fullName">Full Name</label>
+          <input
+            type="text"
+            id="fullName"
+            name="fullName"
+            value={formData.fullName}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="message">Message</label>
+          <textarea
+            id="message"
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+            required
+          ></textarea>
+        </div>
+        <button type="submit">Send</button>
+      </form>
+    </div>
+    </div>
+  );
+};
+
+export default Contact;
